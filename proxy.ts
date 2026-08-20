d8b778c303b8c1e8c4e02da8f157ee9fb846f7d0
ttvlyzard <ttvlyzardyt@gmail.com>
2026-08-20 00:15:21 -0700

    middware update proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { response, user } = await updateSession(request);
   const path = request.nextUrl.pathname;
 
